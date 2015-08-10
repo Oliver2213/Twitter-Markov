d9e0067e99ad10518ef45ad0a46d58834c23c5e1
oliver2213 <oliver22213@me.com>
2015-08-10 07:28:53 -0400

    Moved some code, more work on bot class
    New method in bot class extractTL will dump the tweet text of 200 of the provided user's tweets into a temp file, to which, the returning file path points.
    Then main can use the markov lib to generate the chains dict and randomly come up with one

---
+++ bot.py
@@ -0,0 +1,79 @@
+#This file contains the bot's classes and methods
+
+import tweepy
+import tempfile
+
+class Bot(object):
+    """
+Handles spesific functions of the bot.
+Flow:
+Class should be instantiated with twitter API object
+methods:
+extract_mentions pulls all twitter handles out of the entities sent along with a status update. The resulting list does not have the @ sign attached to each name, this is done in main.
+extractTL pulls the latest 200 tweets from the provided user's timeline and tosses their text into a temp file, then returns that file path and name
+Main can then use that file name with the markov library to generate a markov dict and pull a response out of it
+
+
+"""
+    def __init__(self, twitterAPI):
+        """
+This method initializes the bot class with the twitter API in use, other methods use this value, so when instantiating this class be sure to do it like:
+bot = bot.bot(api)
+then other methods can be accessed and they'll use the right api object
+"""
+        self.twitterAPI = twitterAPI
+
+    def extract_mentions(self, tweet):
+        """Return a list of @mentionnames in the provided tweet object"""
+        self.tweet = tweet
+        self.mentionnames = []
+        for item in self.tweet.entities['user_mentions']:
+            self.mentionnames.append(self.tweet.entities['user_mentions'][item]['screen_name'])
+        return self.mentionnames
+
+    def extractTL(self, userid):
+        """Takes a user's timeline and extracts the text of each tweet into a temp file, then returns the path and name to that file"""
+        self.userid = userid
+        self.tempfile = tempfile.NamedTemporaryFile(prefix=str(self.userid), suffix='.tmp', delete=False)
+        self.temptl = self.twitterAPI.user_timeline(self.userid, count=200)
+        #Gotta be a better way of doing this...
+        self.num=0
+        for item in self.temptl:
+            self.tweettext = self.temptl[item].text
+            self.tweettext = self.tweettext+"\n"
+            self.tempfile.write(tweettext)
+            self.num = self.num+1
+        self.tempfile.flush()
+
+
+class BotStreamListener(tweepy.StreamListener):
+    """Class that handles tweepy streaming events.
+E.g: on_connect, on_disconnect, on_status, on_direct_message, etc."""
+    def on_connect( self ):
+        """Gets run when the stream is first connected to twitter"""
+        print("Connection to twitter established!!")
+        self.me = api.me()
+
+    def on_disconnect( self, notice ):
+        """Gets run when the stream gets disconnected from twitter"""
+        print("Connection to twitter lost!! : ", notice)
+
+    def on_status( self, status ):
+        """Gets run when the stream receives a status update (tweet)"""
+        print(status.user.name+": \""+status.text+"\"")
+        return True
+
+    def on_direct_message(self, status):
+        """Gets run when the stream receives a direct message"""
+        print("Direct message received.")
+        try:
+            #If the DM isn't one that was sent from this account
+            if status.direct_message['sender_screen_name'] != self.me.screen_name:
+                print(status.direct_message['sender_screen_name']+": \""+status.direct_message['text']+"\"")
+        except BaseException as e:
+            print("Failed on_direct_message()", str(e))
+        return True #Keep the stream open
+
+    def on_error( self, status ):
+        print(status)
+
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #Markov chaining twitter bot
 #Author: Blake Oliver <oliver22213@me.com>
-import tweepy
+
+import bot
 import markov
+import tweepy
 from keys import * #import our user keys
 
 #Twitter auth stuff
@@ -9,61 +11,13 @@ auth = tweepy.OAuthHandler(c_key, c_secret)
 auth.set_access_token(token, token_secret)
 api = tweepy.API(auth) # Get our API object
 
-class MarkovBot(object):
-    """This class holds methods that make things much easier for the bot"""
-    def __init__(self, twitterAPI, userid, include_profile=False):
-        """
-This method takes 3 args:
-twitterAPI, which is the api object for this particular account. I almost always have this set to api, but no reason to be inflexible.
-userid, which is a list with the Twitter user ID of the user or users mentioning the bot
-include_profile, which is a boolean value that determines if the bot will include the user's profile description text in the markov chains file.
-The method then calls the tweepy API method to get the last tweets of a user or users, parses that list, and dumps each tweet's text into a textfile.
-"""
-        self.twitterAPI = twitterAPI
-        self.userid = userid
-        self.include_profile = include_profile         
-
-
-
-
-class BotStreamListener(tweepy.StreamListener):
-    """Class that handles tweepy streaming events.
-E.g: on_connect, on_disconnect, on_status, on_direct_message, etc."""
-    def on_connect( self ):
-        """Gets run when the stream is first connected to twitter"""
-        print("Connection to twitter established!!")
-        self.me = api.me()
-
-    def on_disconnect( self, notice ):
-        """Gets run when the stream gets disconnected from twitter"""
-        print("Connection to twitter lost!! : ", notice)
-
-    def on_status( self, status ):
-        """Gets run when the stream receives a status update (tweet)"""
-        print(status.user.name+": \""+status.text+"\"")
-        return True
-
-    def on_direct_message(self, status):
-        """Gets run when the stream receives a direct message"""
-        print("Direct message received.")
-        try:
-            #If the DM isn't one that was sent from this account
-            if status.direct_message['sender_screen_name'] != self.me.screen_name:
-                print(status.direct_message['sender_screen_name']+": \""+status.direct_message['text']+"\"")
-        except BaseException as e:
-            print("Failed on_direct_message()", str(e))
-        return True #Keep the stream open
-
-    def on_error( self, status ):
-        print(status)
-
 
 def main():
 
     try:
         me = api.me()
         print "Starting userstream for %s ( %s )" %(me.name, me.screen_name)
-        stream = tweepy.Stream(auth, BotStreamListener())
+        stream = tweepy.Stream(auth, bot.BotStreamListener())
         #Start the stream
         stream.userstream()
 
