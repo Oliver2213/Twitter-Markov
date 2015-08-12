951fcbea2b22d67e650330e333b0af0326dc450f
oliver2213 <oliver22213@me.com>
2015-08-12 03:22:22 -0400

    Updated Bot.extractTL, added Bot.cleanup

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 #This file contains the bot's classes and methods
 
+import glob
 from tweepy import StreamListener
 import tempfile
 
@@ -18,24 +19,24 @@ Main can then use that file name with the markov library to generate a markov di
     def __init__(self, twitterAPI):
         """
 This method initializes the bot class with the twitter API in use. Other methods use this value, so when instantiating this class be sure to do it like:
-bot = bot.bot(api)
+mybot = bot.bot(api)
 then other methods can be accessed and they'll use the right api object
 """
         self.twitterAPI = twitterAPI
 
     def extract_mentions(self, tweet):
-        """Return a list of @mentionnames in the provided tweet object"""
+        """Accepts a standard tweepy status object, returns a list of @mentionnames in the provided tweet object with out the at (@) sign attached"""
         self.tweet = tweet
         self.mentionnames = []
         for item in self.tweet.entities['user_mentions']:
             self.mentionnames.append(item['screen_name'])
         return self.mentionnames
 
-    def extractTL(self, userid):
+    def extractTL(self, userids):
         """Takes a user's timeline and extracts the text of each tweet into a temp file, then returns the path and name to that file"""
         self.userid = userid
         #Create a temporary file where the extracted tweet text will go
-        self.tempfile = tempfile.NamedTemporaryFile(prefix=str(self.userid), suffix='.tmp', delete=False)
+        self.tempfile = tempfile.NamedTemporaryFile(prefix=str('twitter-markov-'), suffix='.tmp', delete=False)
         #Get the last 200 items in the provided user's timeline
         self.temptl = self.twitterAPI.user_timeline(self.userid, count=200)
         for item in self.temptl:
@@ -45,6 +46,15 @@ then other methods can be accessed and they'll use the right api object
             self.tempfile.write(self.tweettext) # Write our new line
         self.tempfile.flush() # update the file on disk
         self.tempfile.close() # Close the file handle
+        return self.tempfile.name # return the path to the textfile
+
+    def cleanup(self):
+        """Clean any temporary files the bot has created before shutdown"""
+        self.tmpfilelist = glob.glob(tempfile.gettempdir()+'twitter-markov-[a-z0-9].tmp')
+        self.removed=0
+        for item in self.tmpfilelist:
+            self.removed = self.removed+1
+            os.remove(item)
 
 
 class BotStreamListener(StreamListener):
@@ -58,22 +68,18 @@ E.g: on_connect, on_disconnect, on_status, on_direct_message, etc.
         self.bot = bot
 
     def on_connect( self ):
-        """Gets run when the stream is first connected to twitter"""
         print("Connection to twitter established!!")
         self.me = self.api.me() #Store values about the authorized account
 
 
     def on_disconnect( self, notice ):
-        """Gets run when the stream gets disconnected from twitter"""
         print("Connection to twitter lost!! : ", notice)
 
     def on_status( self, status ):
-        """Gets run when the stream receives a status update (tweet)"""
         print(status.user.name+": \""+status.text+"\"")
         return True
 
     def on_direct_message(self, status):
-        """Gets run when the stream receives a direct message"""
         print("Direct message received.")
         try:
             #If the DM isn't one that was sent from this account
